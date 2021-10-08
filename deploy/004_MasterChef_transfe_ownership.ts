dee5efbeec64ba24cdabcc20c84a9df9eb97441b
curdin0xBeets <curdin.beets@protonmail.com>
2021-10-08 15:43:48 +0200

    prepare for ownership transfer

--- deploy/004_MasterChef_transfe_ownership.ts
+++ deploy/004_MasterChef_transfe_ownership.ts
@@ -3,16 +3,17 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 import { BeethovenxMasterChef, BeethovenxToken, Timelock } from "../types"
 
 export default async function ({ ethers, deployments, getNamedAccounts, network }: HardhatRuntimeEnvironment) {
-  const timelockDeployment = await deployments.get("Timelock")
-  const timelock: Timelock = (await ethers.getContractAt("Timelock", timelockDeployment.address)) as Timelock
+  // const timelockDeployment = await deployments.get("Timelock")
+  // const timelock: Timelock = (await ethers.getContractAt("Timelock", timelockDeployment.address)) as Timelock
 
   const masterChef = (await ethers.getContractAt(
     "contracts/BeethovenxMasterChef.sol:BeethovenxMasterChef",
     "0x8166994d9ebBe5829EC86Bd81258149B87faCfd3"
   )) as BeethovenxMasterChef
-  if ((await masterChef.owner()) !== timelock.address) {
+  if ((await masterChef.owner()) !== "0xB5CaEe3CD5d86c138f879B3abC5B1bebB63c6471") {
     // Transfer ownership of MasterChef to timelock
     console.log("Transfer ownership of MasterChef to Timelock")
-    await (await masterChef.transferOwnership(timelock.address)).wait()
+    const tx = await (await masterChef.transferOwnership("0xB5CaEe3CD5d86c138f879B3abC5B1bebB63c6471")).wait()
+    console.log(tx.transactionHash)
   }
 }
--- deploy_orig/003_lp_token_timelock.ts
+++ deploy_orig/003_lp_token_timelock.ts
@@ -3,16 +3,25 @@ import { HardhatRuntimeEnvironment } from "hardhat/types"
 export default async function ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) {
   const { deploy } = deployments
   const { deployer } = await getNamedAccounts()
+
+  /*
+
+        IERC20 token_,
+        address beneficiary_,
+        uint256 releaseTime_,
+        BeethovenxMasterChef masterChef_,
+        uint256 masterChefPoolId_
+   */
   await deploy("MasterChefLpTokenTimelock", {
     from: deployer,
     log: true,
     deterministicDeployment: false,
     args: [
-      "0x33276d43ada054a281d40a11d48310cdc0156fc2",
-      "0xc4a114e1952Cfec7A953Bbe37b815Bb556579807",
-      1633688102,
-      "0x0e317Aa06F6C759a724ecD43548FB77bF5baC5b9",
-      1,
+      "0x03c6B3f09D2504606936b1A4DeCeFaD204687890",
+      "0x0EDfcc1b8D082Cd46d13Db694b849D7d8151C6D5",
+      1650622502000,
+      "0x8166994d9ebBe5829EC86Bd81258149B87faCfd3",
+      0,
     ],
   })
 }
